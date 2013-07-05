ee37470d4281bfc570af168e1b17c87458e33696
scottmotte <scott@scottmotte.com>
2013-07-05 22:08:53 +0000

    retab

--- task.js
+++ task.js
@@ -8,32 +8,32 @@ var sendgrid_username   = process.env.SENDGRID_USERNAME;
 var sendgrid_password   = process.env.SENDGRID_PASSWORD; 
 var fullcontact_key     = process.env.FULLCONTACT_KEY;
 
-var SendGrid    				= require('sendgrid').SendGrid;
-var sendgrid    				= new SendGrid(sendgrid_username, sendgrid_password);
-var fullcontact 				= require('fullcontact-api')(fullcontact_key);
-var redis       				= require('redis');
-var db          				= redis.createClient();
+var SendGrid            = require('sendgrid').SendGrid;
+var sendgrid            = new SendGrid(sendgrid_username, sendgrid_password);
+var fullcontact         = require('fullcontact-api')(fullcontact_key);
+var redis               = require('redis');
+var db                  = redis.createClient();
 
 db.smembers("emails", function(err, data) {
-	var available_emails 	= data;
+  var available_emails  = data;
   var random_number     = Math.floor(Math.random()*available_emails.length);
-	var email 						= available_emails[random_number];
+  var email             = available_emails[random_number];
   
   fullcontact.person.findByEmail(email, function(err, json) {
     if (err) {
-		  console.log(err);
+      console.log(err);
       process.exit();
     } else {
       var photo_url = json.photos[0].url; 
-      var name 			= json.contactInfo.fullName;
+      var name      = json.contactInfo.fullName;
 
-      var html 			= "<p><img src='"+photo_url+"'/></p><ul><li>"+name+"</li></li>";
+      var html      = "<p><img src='"+photo_url+"'/></p><ul><li>"+name+"</li></li>";
 
       sendgrid.send({
-        to 					: to, 
-        from 				: to, 
-        subject 		: '[visage-grid] delivery',
-        html 				: html 
+        to          : to, 
+        from        : to, 
+        subject     : '[visage-grid] delivery',
+        html        : html 
       }, function(success, message) {
         if (!success) {
           console.log(message);
@@ -44,7 +44,7 @@ db.smembers("emails", function(err, data) {
         process.exit();
       });
     }
-	});
+  });
 });
 
 
